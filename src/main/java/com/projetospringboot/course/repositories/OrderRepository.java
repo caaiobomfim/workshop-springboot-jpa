370597c99e8932527c77cde5c72aaca7319eae5a
Caio Bomfim <caaiobomfim@gmail.com>
2023-11-14 17:44:26 -0300

    [PROJECT] - Order, Instant, ISO 8601

---
+++ src/main/java/com/projetospringboot/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.projetospringboot.course.repositories;
+
+import com.projetospringboot.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
