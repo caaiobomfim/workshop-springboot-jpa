89531bae0c13b5dbca033b6b6fd91f5cf8cc9520
Caio Bomfim <caaiobomfim@gmail.com>
2023-11-14 17:15:33 -0300

    [PROJECT] - Order, Instant, ISO 8601

--- src/main/java/com/projetospringboot/course/entities/User.java
+++ src/main/java/com/projetospringboot/course/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 @Entity
 @Table(name = "tb_user")
@@ -18,6 +20,8 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -27,6 +31,10 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public Long getId() {
