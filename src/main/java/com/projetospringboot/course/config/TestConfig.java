751d6892920d0488571ba6ad2d65b3e622a6ee4c
Caio Bomfim <caaiobomfim@gmail.com>
2023-11-14 19:20:47 -0300

    [PROJECT] - Enum OrderStatus

--- src/main/java/com/projetospringboot/course/config/TestConfig.java
+++ src/main/java/com/projetospringboot/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.projetospringboot.course.config;
 
 import com.projetospringboot.course.entities.Order;
 import com.projetospringboot.course.entities.User;
+import com.projetospringboot.course.entities.enums.OrderStatus;
 import com.projetospringboot.course.repositories.OrderRepository;
 import com.projetospringboot.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +27,9 @@ public class TestConfig implements CommandLineRunner {
         User user2 = new User(null, "Davi", "davi@mail.com", "977777777", "123456");
         userRepository.saveAll(Arrays.asList(user1, user2));
 
-        Order order1 = new Order(null, Instant.parse("2023-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2023-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
     }
 }
