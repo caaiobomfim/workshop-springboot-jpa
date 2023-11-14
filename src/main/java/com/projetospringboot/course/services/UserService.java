fc69e24ff9c18acb93575c6dab5d75ccce682f0f
Caio Bomfim <caaiobomfim@gmail.com>
2023-11-14 16:16:52 -0300

    [PROJECT] - endpoint get find all id

--- src/main/java/com/projetospringboot/course/services/UserService.java
+++ src/main/java/com/projetospringboot/course/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +15,10 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
     }
 }
