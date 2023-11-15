825da5ab9d38bce3145b021fb5305e3305729977
Caio Bomfim <caaiobomfim@gmail.com>
2023-11-15 14:44:40 -0300

    [PROJECT] - Delete users

--- src/main/java/com/projetospringboot/course/resources/UserResources.java
+++ src/main/java/com/projetospringboot/course/resources/UserResources.java
@@ -34,4 +34,10 @@ public class UserResources {
                 .path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
